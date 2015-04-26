854abb9af57d295c294f441d3dad2cdef30cee30
Mocanu Valentin <mr_vali@yahoo.ro>
2015-04-26 19:56:43 +0300

    Wrapped it like a plugin. Added some new options: you can hide the close button, you can align text in header, middle and footer.

--- CreateModal.js
+++ CreateModal.js
@@ -1,6 +1,84 @@
-// hide modal manually: $('#mercuryModal-' + ($('.mercuryModal').length - 1)).modal('hide');
-function MercuryModal(options) {
-    var defaults = {
+;(function($) {
+    $.MercuryModal = function (elem, options){
+        var settings = $.extend(true, {}, $.MercuryModal.defaults, options);
+        if(elem && typeof elem == 'object'){
+            if(!settings.text) settings.text = '';
+            settings.text += $(elem).html();
+        }       
+        var length = $('.modal').length;
+        var buttonsLength = $('.'+ settings.id +'-button').length;
+        
+        var modalContent = '<div class="modal fade '+ settings.id +'" id="'+ settings.id +'-'+ length +'" role="dialog" aria-labelledby="modal" aria-hidden="false" style="z-index: '+ (1050 + 10 * length) +'">';
+        modalContent += ' <div class="modal-dialog" style="max-width: '+ settings.width +';"> <div class="modal-content">';
+        if(settings.show.header) {
+            modalContent += '<div class="modal-header" style="text-align: '+ settings.textAlign.header +';';
+            if(settings.show.footer == false && !settings.text) {
+                modalContent += 'border-bottom: 0 solid transparent;';
+            }
+            modalContent += '">';
+
+            if(settings.show.closeButton) {
+                modalContent += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';
+            }
+
+            if (settings.title) {
+                modalContent += '<h4 class="modal-title">';
+                modalContent += settings.title;
+                modalContent += '</h4>';
+            }
+
+            modalContent += '</div>';
+        }
+        if (settings.text) {
+            modalContent += '<div class="modal-body" style="text-align: '+ settings.textAlign.middle +';">';
+            modalContent += settings.text;
+            modalContent += '</div>';
+            if(settings.show.footer) {
+                modalContent += '<div class="modal-footer" style="text-align: '+ settings.textAlign.footer +';">';
+            }
+        }
+        else if(settings.show.footer) {
+            modalContent += '<div class="modal-footer" style="border-top:0; text-align: '+ settings.textAlign.footer +';">';
+        }
+        $.each(settings.buttons, function(index, value){
+            if (value.text) {
+                modalContent += '<button type="button" id="'+ settings.id +'-button-'+ (index + buttonsLength) +'" class="'+ settings.id +'-button btn '+ value.class +'"';
+                if(value.dismiss){
+                    modalContent += ' data-dismiss="modal"';
+                }
+                modalContent += '>';
+                modalContent += value.text;
+                modalContent += '</button>';
+            }
+        });
+        modalContent += '</div> </div> </div> </div>';
+        
+        $('body').append(modalContent);
+        var modalDiv = $('#'+ settings.id +'-'+ length);
+        modalDiv.modal();
+
+        $.each(settings.buttons, function(index, value){
+            if(value.text){
+                if (typeof value.click == 'function') {
+                    modalDiv.on('click', '#'+ settings.id +'-button-'+ (index + buttonsLength), function(){
+                        value.click($(this));
+                    });
+                }
+            }
+        });
+        
+        modalDiv.on('hidden.bs.modal', function(e){
+            settings.hide(e);
+            $(this).data('bs.modal', null);
+            $(this).remove();
+        });
+        
+        $($('.modal-backdrop:not(.backdrop-'+ settings.id +')')[0]).css('z-index', (1050 + 10 * (length - 1) + 1)).addClass('backdrop-'+ settings.id);
+        
+        settings.ready();
+        return this;
+    }
+    $.MercuryModal.defaults = {
         id: 'mercuryModal',
         buttons:[
         {
@@ -16,78 +94,51 @@ function MercuryModal(options) {
             dismiss: true
         }
         ],
-        header: true,
-        footer: true,
+        show: {
+            header: true,
+            footer: true,
+            closeButton: true
+        },
         title: null,
         text: null,
-        textAlign: 'left',
+        textAlign: {
+            header: 'left',
+            middle: 'left',
+            footer: 'left'
+        },
         width: '600px',
         ready: function(){},
         hide: function(){}
+    };
+    $.MercuryModal.close = function(selector){
+        if(!selector || !selector.length) selector = $.MercuryModal.defaults.id;
+        $(selector).modal('hide');
     }
-    
-    var settings = $.extend(true, defaults, options);
-    
-    var length = $('.' + settings.id).length;
-    var buttonsLength = $('.'+ settings.id +'-button').length;
-    
-    var modalContent = '<div class="modal fade '+ settings.id +'" id="'+ settings.id +'-'+ length +'" role="dialog" aria-labelledby="modal" aria-hidden="false" style="z-index: '+ (1050 + 10 * length) +'">';
-    
-    modalContent += ' <div class="modal-dialog" style="max-width: '+ settings.width +';"> <div class="modal-content">';
-    if(settings.header) {
-        modalContent += '<div class="modal-header">';
-        modalContent += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';
-        if (settings.title) {
-            modalContent += '<h4 class="modal-title">';
-            modalContent += settings.title;
-            modalContent += '</h4>';
-        }
-        modalContent += '</div>';
+    $.MercuryModal.closeLast = function(selector){
+        if(!selector || !selector.length) selector = $.MercuryModal.defaults.id;
+        $('#'+ selector +'-' + ($('.'+ selector).length - 1)).modal('hide');
     }
-    if (settings.text) {
-        modalContent += '<div class="modal-body" style="text-align: '+ settings.textAlign +';">';
-        modalContent += settings.text;
-        modalContent += '</div>';
-        if(settings.footer) modalContent += '<div class="modal-footer" style="text-align:center;">';
+    $.MercuryModal.closeAll = function(selector){
+        if(!selector || !selector.length) selector = $.MercuryModal.defaults.id;
+        $('.'+ selector).modal('hide');
     }
-    else{
-        if(settings.footer) modalContent += '<div class="modal-footer" style="border-top:0; text-align:center;">';
+
+    // Helpers
+    $.fn.MercuryModal = function(options) {
+        return this.each(function() {
+            $.MercuryModal(this, options);
+        });
+
     }
-    
-    $.each(settings.buttons, function(index, value){
-        if (value.text) {
-            modalContent += '<button type="button" id="'+ settings.id +'-button-'+ (index + buttonsLength) +'" class="'+ settings.id +'-button btn '+ value.class +'"';
-            value.dismiss ? modalContent += ' data-dismiss="modal"' : modalContent += '';
-            modalContent += '>';
-            modalContent += value.text;
-            modalContent += '</button>';
-        }
-    });
-    
-    modalContent += '</div> </div> </div> </div>';
-    
-    $('body').append(modalContent);
-    var modalDiv = $('#'+ settings.id +'-'+ length);
-    modalDiv.modal();
-    
-    $.each(settings.buttons, function(index, value){
-        if(value.text){
-            if (typeof value.click == 'function') {
-                modalDiv.on('click', '#'+ settings.id +'-button-'+ (index + buttonsLength), function(){
-                    value.click($(this));
-                });
-            }
-        }
-    });
-    
-    modalDiv.on('hidden.bs.modal', function(e){
-        settings.hide(e);
-        $(this).data('bs.modal', null);
-        $(this).remove();
-    });
-    
-    $($('.modal-backdrop:not(.backdrop-'+ settings.id +')')[0]).css('z-index', (1050 + 10 * (length - 1) + 1)).addClass('backdrop-'+ settings.id);
-    
-    settings.ready();
-    return this;
-}
+    $.fn.closeModal = function(){
+        return this.each(function() {
+            $(this).modal('hide');
+        });
+    }
+    window.MercuryModal = function MercuryModal(options) {
+        return $.MercuryModal(null, options);
+    }
+    window.CloseLastModal = function CloseLastModal(selector) {
+        return $.MercuryModal.closeLast(selector);
+    }
+})(jQuery);
