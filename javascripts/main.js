d7a1a5faf94ac9501b8ea3bdc9bbb1c76503d64e
Mocanu Valentin <mr_vali@yahoo.ro>
2015-04-29 22:08:31 +0300

    Demo page

---
+++ javascripts/main.js
@@ -0,0 +1,13 @@
+$('.test').MercuryModal({
+    title: 'This message is from a div',
+    show:{
+        footer: false,
+        closeButton: false
+    }
+});
+var testModal = MercuryModal({
+    title: 'Test',
+    show:{
+        footer: false
+    }
+});
